a7f1590f6dae98b07fbeab09c099727c87564a02
divakaivan <54508530+divakaivan@users.noreply.github.com>
2019-12-05 22:45:56 +0000

    Getting user input, onClick show the result(chat).

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,44 @@
 import React from "react";
 import "../Stylesheets/form.css";
 
+const chatObj = {
+    "Accounting": "link_to_accounting",
+    "Computer Science": "link_to_cs"
+};
+
 class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userSearch: "",
+            submitted: false
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleSubmit(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleClick(e) {
+        e.preventDefault();
+        this.setState({
+            submitted: true
+        })
+    }
+
     render() {
         return (
             <div>
-                <form className="form">
+                <form className="form" onSubmit={this.handleSubmit}>
                     <label>Enter your major:</label>
-                    <input className="formInput" type="text"/>
-                    <button className="formSearchBtn">Search</button>
+                    <input className="formInput" name="userSearch" type="text" value={this.state.userSearch} onChange={this.handleSubmit}/>
+                    <input className="formSearchBtn" type="submit" onClick={this.handleClick}/>
                 </form>
+                {this.state.submitted ? <p>{chatObj[this.state.userSearch]}</p> : null}
             </div>
         );
     }
